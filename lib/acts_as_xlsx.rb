f736728f6169e26fae76e65bdf456d5af10786c7
Randy Morgan <digital.ipseity@gmail.com>
2012-07-27 09:40:55 +0900

    only register the mime type if it is undefined

--- lib/acts_as_xlsx.rb
+++ lib/acts_as_xlsx.rb
@@ -1,7 +1,9 @@
 require 'acts_as_xlsx/ar.rb'
 begin
   # The mime type to be used in respond_to |format| style web-services in rails
-  Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+  unless defined? Mime::XLSX
+    Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+  end
 rescue NameError
   puts "Mime module not defines. Skipping registration of xlsx"
 end
